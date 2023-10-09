486a0fed47e0c22506fbbb911f41ae6b3782ed9f
Lucas Eduardo Batista Alves <95626902+LukasEduardo@users.noreply.github.com>
2023-10-09 20:29:22 -0300

    Alimento agora salvei
    kkkkkkkkkkkkkkk

--- src/nerdfitness/Alimento.java
+++ src/nerdfitness/Alimento.java
@@ -4,10 +4,82 @@
  */
 package nerdfitness;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author Alunos
+ * @author Lukas
  */
 public class Alimento {
+    static int idalimento;
+    private String nome;
+    private int proteina;
+    private int carboidrato;
+    private int gordura;
+    private int calorias;
+    
+    
+    
+    
+    //Contrutor do alimento testandp
+    public Alimento (String nome,int proteina, int carboidrato, int gordura)
+    {
+        this.nome = nome;
+        this.proteina = proteina;
+        this.carboidrato = carboidrato;
+        this.gordura = gordura;
+        
+        //Calculo de caloria do alimento
+        this.calorias = ((this.gordura*9)+(this.proteina*4)+(this.carboidrato*4));
+        //Contador de alimentos adicionadps( Id ) 
+        this.idalimento++;
+        
+        
+    }
+
+    
+  
+    //Getters
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getProteina() {
+        return proteina;
+    }
+
+    public int getCarboidrato() {
+        return carboidrato;
+    }
+
+    public int getGordura() {
+        return gordura;
+    }
+
+    public int getCalorias() {
+        return calorias;
+    }
+
+    public void setProteina(int proteina) {
+        this.proteina = proteina;
+    }
+    
+    //setters
+
+    public void setCarboidrato(int carboidrato) {
+        this.carboidrato = carboidrato;
+    }
+
+    public void setGordura(int gordura) {
+        this.gordura = gordura;
+    }
+
+    public void setCalorias(int calorias) {
+        this.calorias = calorias;
+    }
+    
+    
     
 }
