6066419b276f355200886e3196994660fff4fcaf
Lucas Eduardo Batista Alves <95626902+LukasEduardo@users.noreply.github.com>
2023-10-09 21:15:39 -0300

    Data de Modificação Alimento

--- src/nerdfitness/Alimento.java
+++ src/nerdfitness/Alimento.java
@@ -4,6 +4,7 @@
  */
 package nerdfitness;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 /**
@@ -17,6 +18,8 @@ public class Alimento {
     private int carboidrato;
     private int gordura;
     private int calorias;
+    private LocalDate dataCriacao;
+    private LocalDate datamod;
     
     
     
@@ -24,26 +27,27 @@ public class Alimento {
     //Contrutor do alimento testandp
     public Alimento (String nome,int proteina, int carboidrato, int gordura)
     {
+        this.dataCriacao = LocalDate.now();
         this.nome = nome;
         this.proteina = proteina;
         this.carboidrato = carboidrato;
-        this.gordura = gordura;
-        
+        this.gordura = gordura;    
         //Calculo de caloria do alimento
         this.calorias = ((this.gordura*9)+(this.proteina*4)+(this.carboidrato*4));
         //Contador de alimentos adicionadps( Id ) 
         this.idalimento++;
-        
-        
+           
+    }
+    //Atualizar data de modificação
+        private void atualizarData() {
+        this.datamod = LocalDate.now();
     }
 
-    
-  
     //Getters
     
-
     public String getNome() {
         return nome;
+
     }
 
     public int getProteina() {
@@ -70,15 +74,21 @@ public class Alimento {
 
     public void setCarboidrato(int carboidrato) {
         this.carboidrato = carboidrato;
+        atualizarData();
     }
 
     public void setGordura(int gordura) {
         this.gordura = gordura;
+        atualizarData();
     }
 
     public void setCalorias(int calorias) {
         this.calorias = calorias;
+        atualizarData();
     }
+    
+ 
+
     
     
     
