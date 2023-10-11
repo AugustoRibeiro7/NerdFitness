dfd08aa7b58ca985c28f9535f4179a72f6cb5ffe
Lucas Eduardo Batista Alves <95626902+LukasEduardo@users.noreply.github.com>
2023-10-11 15:54:04 -0300

    Dieta

--- src/Classes/Dieta.java
+++ src/Classes/Dieta.java
@@ -9,5 +9,52 @@ package Classes;
  * @author Lukas
  */
 public class Dieta {
+    private String tipo;
+    private int idDieta;
+    private String nome;
+    private int carboidrato;
+    private int proteina;
+    private int gordura;
+    private Double imc;
+    //Adicionar data de Criação e modificação  
     
+    //Contrutor da dieta
+    public Dieta(String tipo, Pessoa pessoa,AvaliacaoFisica avaliacao){
+        
+        //ATRIBUTOS REFERENTE A DIETA
+        
+        
+        // Loop infinito
+        for (; ; ) {  
+            if (tipo.equals("DIMINUIR O PESO") || tipo.equals("MANTER O PESO") ||
+                tipo.equals("MELHORAR COMPOSIÇÃO CORPORAL") || tipo.equals("AUMENTAR O PESO")) {
+                this.tipo = tipo;  // Atribui o tipo válido
+                break;  // Sai do loop quando o tipo é válido
+            } else {
+                System.out.println("TIPO INVÁLIDO");
+
+            }
+            
+            //ATRIBUTOS REFERENTE A PESSOA
+            nome = pessoa.getNome();
+            
+            
+            //ATRIBUTOS REFERENTE A AVALIAÇÃO FISICCA
+            //imc= avaliacao.imc()
+            
+            
+            
+            
+            
+            
+            
+            
+            //ATRIBUTOS REFERENTE A AVALIACAO FISICA
+        
+
+        
+        }
+    
+    
+    }
 }
